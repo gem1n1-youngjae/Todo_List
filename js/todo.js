c16d4ff9c70acba78fc293918ddc2be748f9b9c2
pdlma0904 <a01094741871@gmail.com>
2020-05-10 09:53:43 +0900

    studing

--- js/todo.js
+++ js/todo.js
@@ -5,40 +5,61 @@ const toDoForm = document.querySelector(".js-toDoForm"),
 const TODOS_LS = "toDos";
 
 let toDos = [];
+let space = []
 
 function deleteToDo(event) {
   const btn = event.target;
   const li = btn.parentNode;
+  const div = btn.parentNode;
   toDoList.removeChild(li);
-  const cleanToDos = toDos.filter(function(toDo) {
+  toDoList.remove(div);
+  const cleanli = toDos.filter(function(toDo) {
     return toDo.id !== parseInt(li.id);
   });
-  toDos = cleanToDos;
+  const cleandiv = space.filter(function(div) {
+    return div.id !== parseInt(div.id);
+  });
+  toDos = cleanli;
+  space = cleandiv
   saveToDos();
+  savediv();
 }
 
 function saveToDos() {
   localStorage.setItem(TODOS_LS, JSON.stringify(toDos));
 }
 
+function savediv(){
+  localStorage.setItem(TODOS_LS, JSON.stringify(space));
+}
+
 function paintToDo(text) {
   const li = document.createElement("li");
   const delBtn = document.createElement("button");
   const span = document.createElement("span");
+  const div = document.createElement("div");
   const newId = toDos.length + 1;
   delBtn.innerText = "❌";
   delBtn.addEventListener("click", deleteToDo);
   span.innerText = text;
-  li.appendChild(delBtn);
   li.appendChild(span);
+  li.appendChild(delBtn);
   li.id = newId;
   toDoList.appendChild(li);
+  div.id = newId;
+  div.classList.add("space");
+  toDoList.appendChild(div);
   const toDoObj = {
     text: text,
     id: newId
   };
+  const setDiv = {
+    id: newId
+  };
   toDos.push(toDoObj);
+  space.push(setDiv);
   saveToDos();
+  savediv();
 }
 
 function handleSubmit(event) {
